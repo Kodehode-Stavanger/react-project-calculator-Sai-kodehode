eb5fcb93e89f66c77a97d61328108f94db7011d7
Sai Kodehode <sai.kodehode@gmail.com>
2024-04-10 19:03:07 +0200

    added workflow file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "Kodehode-Stavanger/react-project-calculator-Sai-kodehode",
 });
